448abe611a0666c194bfedcc602e15ecbde974e6
Cesar Frick <frickcesar@gmail.com>
2017-11-12 13:17:39 -0500

    Data-binding excercise

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent {
+  username = '';
+
+  resetUsername() {
+    this.username = '';
+  }
+
+}
